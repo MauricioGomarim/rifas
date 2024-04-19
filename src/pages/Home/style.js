f42c4325915b2047fdc3d9451554890c93007922
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-04-18 23:36:26 -0300

    fix bug

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -10,7 +10,7 @@ export const Container = styled.div`
   .container-home {
     max-width: 600px;
     margin: 0 auto;
-    height: 100%;
+    height: 100vh;
     background-color: #181818;
     padding: 14px;
     border-radius: 20px;
