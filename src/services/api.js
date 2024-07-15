2f5dd6c0f9982ad879d96fc2b6db893c77f303f2
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-07-14 23:45:13 -0300

    test

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://backend-rifa-mauriciogomarimrifa-35d24eb0.koyeb.app" s
+    baseURL: "https://backend-rifa-mauriciogomarimrifa-35d24eb0.koyeb.app"
 })
 
 
